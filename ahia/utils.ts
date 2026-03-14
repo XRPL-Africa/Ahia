5475c7433edbc60b9fd1103112e79058e1b40462
B0SS-C1AN0 <louiscolix@gmail.com>
2026-03-14 07:25:07 +0100

    Delete ahia directory

--- ahia/utils.ts
+++
@@ -1,7 +0,0 @@
-export function triggerHaptic(pattern: number[]): void {
-  if (navigator.vibrate) navigator.vibrate(pattern);
-}
-
-export function formatNaira(amount: number): string {
-  return "₦" + amount.toLocaleString("en-NG", { maximumFractionDigits: 0 });
-}
